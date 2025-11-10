3cdc827e9182cdeafbedd412f1b4a39248f1b235
130oclock <aodan13@att.net>
2025-11-10 13:43:08 -0500

    updated test job

--- LIT_PCBA_job.sh
+++ LIT_PCBA_job.sh
@@ -22,5 +22,20 @@ module load cuda/11.8.0
 # Specify the script you want to run
 cd /scratch/eecs498f25s006_class_root/eecs498f25s006_class/shared_data/group2_DrugCLIP/eecs498-DrugCLIP
 source ./.venv/bin/activate
-python ./unimol/test.py --user-dir ./unimol $data_path "./data" --valid-subset test --results-path ./test --num-workers 8 --ddp-backend=c10d --batch-size 8 --task drugclip --loss in_batch_softmax --arch drugclip --fp16 --fp16-init-scale 4 --fp16-scale-window 256 --seed 1 --path checkpoint_best.pt --log-interval 100 --log-format simple --max-pocket-atoms 511 --test-task PCBA
+
+results_path="./test"  # replace to your results path
+batch_size=8
+weight_path="checkpoint_best.pt"
+
+TASK="PCBA" # DUDE or PCBA
+
+CUDA_VISIBLE_DEVICES="0" python ./unimol/test.py --user-dir ./unimol $data_path "./data" --valid-subset test \
+       --results-path $results_path \
+       --num-workers 8 --ddp-backend=c10d --batch-size $batch_size \
+       --task drugclip --loss in_batch_softmax --arch drugclip  \
+       --fp16 --fp16-init-scale 4 --fp16-scale-window 256  --seed 1 \
+       --path $weight_path \
+       --log-interval 100 --log-format simple \
+       --max-pocket-atoms 511 \
+       --test-task $TASK \
 
